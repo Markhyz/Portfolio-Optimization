bcaf990f5861de95ea7af3541267e3da5324071f
Felipe Marchi <felipe.r.marchi@gmail.com>
2019-03-26 18:19:27 -0300

    added more ibovespa data

--- Ibovespa/stockReturn.py
+++ Ibovespa/stockReturn.py
@@ -1,9 +1,10 @@
 import os
+import sys
 
 for root, dirs, files in os.walk(sys.argv[1]):
     for file_name in files:
-        with open("ativos/" + file_name, 'r') as in_f:
-            with open("input/" + file_name + ".in", 'w') as out_f:
+        with open(sys.argv[1] + "/" + file_name, 'r') as in_f:
+            with open(sys.argv[2] + "/" + file_name + ".in", 'w') as out_f:
                 first_line = True
                 for line in in_f:
                     if first_line:
